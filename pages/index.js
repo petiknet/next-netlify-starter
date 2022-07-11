c65bb1598903719716a3cb5b1f97fd25865a5fbf
Petiknet <105087317+petiknet@users.noreply.github.com>
2022-07-11 10:33:48 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,16 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+    <a href="https://petik.net" title="Petiknet">
+    <img src="https://petik.net/wp-content/uploads/2022/07/new-logos.webp" width="350" height="65" alt="Petiknet">
+    </a>
+        <Header title="PETIKNET" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Petiknet adalah situs yang menyajikan informasi terkini di Indonesia dan dunia seputar Teknologi, Digital , Bisnis, Game, Hingga Edukasi yang bermanfaat dan juga menyajikan tutorial menarik dan berita berita seru lainnya.
+
+
+Ada banyak informasi tentang digital di media sosial tetapi tidak ada yang memastikan bahwa Anda membaca hal-hal yang bermanfaat di akun media sosial Anda, Anda dapat mengunjungi situs <a href="https://bit.ly/petiknet" title="PetikNet">PetikNet</a> untuk mendapatkan pengetahuan umum. Dapatkan Informasi Lowongan Kerja Terbaru & terupdate. Info Loker Gratis hanya di <a href="https://bit.ly/kerjabos" title="Kerjabos">Kerjabos</a>
+    
         </p>
       </main>
 
